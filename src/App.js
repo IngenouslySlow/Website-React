b4d61ceebfba795f7ff6db51321b6df9f5cf79e9
Being Cryptiic <krishnasaimarupudi114@gmail.com>
2021-05-29 09:37:28 +0530

    Style AboutSection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 //Importing pages
 import AboutUs from "./Pages/AbousUs";
-
+//Global styles import
+import GlobalStyles from "./Components/GlobalStyles";
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <AboutUs />
     </div>
   );
---
+++ src/Components/GlobalStyles.js
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}
+body{
+    background:#1b1b1b;
+}
+button{
+    font-weight:bold;
+    font-size:1.1rem;
+    cursor:pointer;
+    padding:0.75rem 1.5rem;
+    border:3px solid #23d997;
+    background:transparent;
+    color:white;
+    transition:all 0.5s ease;
+    &:hover{
+        background:#23d997;
+        color:white;
+    }
+}
+h2{
+font-weight:lighter;
+font-size:3rem;
+}
+h3{
+color:white;
+}
+h4{
+font-weight:bold;
+}
+span{
+font-weight:bold;
+color:#23d997;
+}
+a{
+font-size:1.1rem;
+}
+p{
+padding:3rem 0;
+color:gray;
+font-size:1.2rem;
+line-height:150%
+}
+`;
+export default GlobalStyles;
